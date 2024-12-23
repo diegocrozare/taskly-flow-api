24b58f4dafedc5c5c5f11d9ef784255293f49397
diegocrozare <crozare.crozare@hotmail.com>
2024-12-23 12:29:49 -0500

    feat: adicionado service de task

---
+++ src/main/kotlin/com/enlevo/taskly_flow/controller/TasksController.kt
@@ -0,0 +1,4 @@
+package com.enlevo.taskly_flow.controller
+
+class TasksController {
+}
--- src/main/kotlin/com/enlevo/taskly_flow/service/TaskService.kt
+++ src/main/kotlin/com/enlevo/taskly_flow/service/TaskService.kt
@@ -1,4 +1,14 @@
 package com.enlevo.taskly_flow.service
 
-class TaskService {
+import com.enlevo.taskly_flow.domain.Task
+import com.enlevo.taskly_flow.repository.TaskRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TaskService(private val repository: TaskRepository) {
+
+    fun addTask(task: Task): Task {
+        return repository.save(task)
+    }
+
 }
