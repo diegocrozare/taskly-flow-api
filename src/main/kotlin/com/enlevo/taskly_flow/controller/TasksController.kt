617fe1f517eb03ee43d8ac1d025ddbd65a3205f2
diegocrozare <crozare.crozare@hotmail.com>
2024-12-23 12:49:09 -0500

    feat: Adicionado camada da controller para a feature tasks

--- src/main/kotlin/com/enlevo/taskly_flow/controller/TasksController.kt
+++ src/main/kotlin/com/enlevo/taskly_flow/controller/TasksController.kt
@@ -1,4 +1,28 @@
 package com.enlevo.taskly_flow.controller
 
-class TasksController {
+import com.enlevo.taskly_flow.domain.Task
+import com.enlevo.taskly_flow.service.TaskService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
+
+@RestController
+@RequestMapping("/tasks")
+class TasksController(private val service: TaskService) {
+
+    @PostMapping
+    fun addTask(@RequestBody task: Task, uriBuilder: UriComponentsBuilder): ResponseEntity<Task> {
+        val createdTask = service.addTask(task)
+
+        val location = uriBuilder
+            .path("/tasks/{id}")
+            .buildAndExpand(createdTask.id)
+            .toUri()
+
+        return ResponseEntity.created(location).body(createdTask)
+    }
+
 }
