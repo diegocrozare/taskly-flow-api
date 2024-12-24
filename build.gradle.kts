5db5a97506729b81468b793b6fb891300cdcbd08
diegocrozare <crozare.crozare@hotmail.com>
2024-12-24 12:44:27 -0500

    feat: Adicionado documentacao swagger ao projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.openapi.generator") version "6.6.0"
 }
 
 noArg {
@@ -31,6 +32,8 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
